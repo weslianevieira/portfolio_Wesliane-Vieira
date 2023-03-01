74c15c3487f3d967132773d75290fa5c48b84da3
Wesliane Vieira <weslianebsb2@gmail.com>
2023-03-01 10:34:11 -0300

    completed about me section

---
+++ TO_DO.js
@@ -0,0 +1,22 @@
+/*
+  *
+  *
+  * 
+
+DONE: header and home -> html css animation
+DONE: 
+
+DOING: about section -> html css
+DOING: 
+DOING: 
+
+TODO: webpage multi-language -> html css jquery
+TODO: responsivity -> header home about skills projects contact footer
+TODO: increment dowload CV button
+TODO: 
+TODO: 
+
+  * 
+  *
+  * 
+*/
